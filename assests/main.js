3839b1893e21846890c2b128f1d14c9650f2141b
NisaNajafli <ncfliniss@gmail.com>
2026-03-26 21:39:59 +0400

    new

--- assests/main.js
+++ assests/main.js
@@ -3,3 +3,8 @@ document.getElementById("year").textContent = new Date().getFullYear();
 document.querySelector('.scroll-top').onclick = () => {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 };
+
+const mainButton = document.getElementById("main-button");
+mainButton.addEventListener("click", function () {
+  this.classList.toggle("open");
+});
