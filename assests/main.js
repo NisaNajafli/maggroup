c8f78961bc888efe0f9d30cd5617735180b362db
NisaNajafli <ncfliniss@gmail.com>
2026-04-02 12:34:09 +0400

    new

--- assests/main.js
+++ assests/main.js
@@ -8,3 +8,32 @@ const mainButton = document.getElementById("main-button");
 mainButton.addEventListener("click", function () {
   this.classList.toggle("open");
 });
+
+const modal = document.querySelector(".modal");
+
+// OPEN
+document.querySelectorAll("[data-open-modal]").forEach(btn => {
+  btn.addEventListener("click", () => {
+    modal.classList.add("active");
+    document.body.style.overflow = "hidden";
+  });
+});
+
+// CLOSE
+document.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("modal-overlay") ||
+    e.target.classList.contains("modal-close")
+  ) {
+    modal.classList.remove("active");
+    document.body.style.overflow = "";
+  }
+});
+
+// ESC
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    modal.classList.remove("active");
+    document.body.style.overflow = "";
+  }
+});
